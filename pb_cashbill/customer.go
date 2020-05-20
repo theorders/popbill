382e92281e29452c67c20ea4f70e46a1c7de0d25
taeuk.kang <taeuk.kang@theorders.co>
2020-05-20 10:44:19 +0900

    customer email

--- pb_cashbill/customer.go
+++ pb_cashbill/customer.go
@@ -2,8 +2,9 @@ package pb_cashbill
 
 type Customer struct {
 	Item
-	
+
 	TradeUsage   TradeUsage `json:"tradeUsage" firestore:"tradeUsage"`
 	IdentityNum  string     `json:"identityNum" firestore:"identityNum"`
 	CustomerName string     `json:"customerName" firestore:"customerName"`
+	Email        string     `json:"email,omitempty" firestore:"email,omitempty"`
 }
